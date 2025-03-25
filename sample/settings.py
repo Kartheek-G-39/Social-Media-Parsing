96730e56d960cb9f6f92b37ff3c70d34484d1a51
Kartheek-G-39 <21bq1a4239@vvit.net>
2025-03-26 02:15:47 +0530

    ok

--- sample/settings.py
+++ sample/settings.py
@@ -139,9 +139,10 @@ import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static'),
+    os.path.join(BASE_DIR, 'app', 'static'),  # Remove the leading slash
 ]
+
